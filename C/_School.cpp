ea22e9471ce4c767f45434d64c66e42b43a53023
xTaiwanPingLord <tojimmy0917@gmail.com>
2022-10-18 15:21:10 +0800

    221018

--- C/_School.cpp
+++ C/_School.cpp
@@ -1,32 +1,74 @@
-#include<stdio.h>
-
-void Sort(int *array1, int len)
+#include <stdio.h>
+#include <stdlib.h>
+void QuickSort(int *data, int left, int right)
 {
-    int min=0;//最小位置 
-	int temp;
-
-	for(int i=0;i<len-1;i++){
-		min=i;
-		for(int j=i;j<len;j++){
-			if(array1[j]<array1[min]){
-				min=j;		
+	if (left >= right)
+	{
+		return;
+	}
+	else
+	{
+		int pivot = data[left];
+		int i = left + 1;
+		int j = right;
+		while (1)
+		{
+			while (i <= right)
+			{
+				if (pivot < data[i])
+				{
+					break;
+				}
+				i++;
 			}
+
+			while (j > left)
+			{
+				if (pivot > data[j])
+				{
+					break;
+				}
+				j--;
+			}
+			if (i >= j)
+			{
+				break;
+			}
+			int temp = data[i];
+			data[i] = data[j];
+			data[j] = temp;
 		}
-		temp=array1[i];
-		array1[i]=array1[min];
-		array1[min]=temp;	
+
+		int temp = data[j];
+		data[j] = data[left];
+		data[left] = temp;
+		QuickSort(data, left, j - 1);
+		QuickSort(data, j + 1, right);
 	}
 }
 
+int main()
+{
+	int i, n, data[10];
 
-int main(){
+	printf("請輸入資料筆數 n(<= 10): ");
+	scanf("%d", &n);
 
-	int array[]={8,7,2,4,6},len=5;
-    Sort(array, len);
-    for(int i=0;i<len;i++){
+	for (i = 0; i < n; i++)
+	{
+		printf("請輸入第 %d 筆資料: ", i + 1);
+		scanf("%d", &data[i]);
+	}
 
-		printf("%d ",array[i]);
+	// 執行快速排序法
+	QuickSort(data, 0, n - 1);
 
-	}	
+	printf("\n排序後的結果: ");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", data[i]);
+	}
 
+	printf("\n");
+	system("pause");
 }
