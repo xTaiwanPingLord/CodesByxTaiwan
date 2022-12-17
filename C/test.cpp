6a380a83a8839c2ef24bdbb7bfef22756f9d821f
xTaiwanPingLord <tojimmy0917@gmail.com>
2022-12-17 20:13:21 +0800

    apcs

--- C/test.cpp
+++ C/test.cpp
@@ -3,16 +3,24 @@ using namespace std;
 
 int main()
 {
-    int a, b, c, d;
-    a = int(1e+9);
-    b = int(1.2e+9);
-    c = int(1.1e+9);
-    d = int(1.3e+9);
-    cout << a << endl;
-    cout << (a * b < c * d) << endl;
-    cout << a * b << " " << c * d << endl;
-    cout << uint64_t(a) * uint64_t(b) << " " << uint64_t(c) * uint64_t(d) << endl;
-    cout << (uint64_t(a) * uint64_t(b) < uint64_t(c) * uint64_t(d));
+    vector<int> my_vector(5, 10);
 
-    return 0;
+    for (auto i : my_vector)
+    {
+        cout << " " << i;
+    }
+    cout << " " << *my_vector.end();
+    my_vector.erase(my_vector.end());
+
+    for (auto i : my_vector)
+    {
+        cout << " " << i;
+    }
+
+    my_vector.insert(my_vector.begin() + 1, 15);
+
+    for (auto i : my_vector)
+    {
+        cout << " " << i;
+    }
 }
