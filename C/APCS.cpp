7f05a19710ca4378822098bc401bd1c624d6e3d1
xTaiwanPingLord <tojimmy0917@gmail.com>
2022-12-21 23:31:33 +0800

    sping

--- C/APCS.cpp
+++ C/APCS.cpp
@@ -1,62 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
+// 黑桃 A 歸零
+// A +1
+// 2 +2
+// 3 +3
+// 4 迴轉 (如本來是順時針出牌改成逆時針出牌)
+// 5 跳過一輪
+// 6 +6
+// 7 +7
+// 8 +8
+// 9 +9
+// 10 −10
+// J 跳過一輪
+// Q −20
+// K 加到 99
+// S 是黑桃，H 是紅心，D 是方塊，C 是梅花
 
-void InputVector(vector<vector<int>> &small, vector<vector<int>> &big, int &area)
-{
-    for (auto &i : small)
-    {
-        for (auto &j : i)
-        {
-            cin >> j;
-        }
-    }
-    for (auto &i : big)
-    {
-        for (auto &j : i)
-        {
-            cin >> j;
-            area += j;
-        }
-    }
-}
+queue<string> card_stack;
+map<char, vector<string>> player_priority, player_card;
+vector<int> score;
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int r1, c1, r2, c2, A = 0, max_delta, min_delta = 100000, delta = 0, sum_delta = 0, num_delta = 0, num_submatrix = 0;
-    cin >> r1 >> c1 >> r2 >> c2 >> max_delta;
-    vector<vector<int>> small(r1, vector<int>(c1, 0)), big(r2, vector<int>(c2, 0));
-
-    InputVector(small, big, A);
-
-    for (int i = 0; i < int(big.size() - small.size() + 1); i++)
+    for (int i = 0; i < 52; i++)
     {
-        for (int j = 0; j < int(big[i].size() - small[0].size() + 1); j++)
-        {
-            sum_delta = 0;
-            num_delta = 0;
-            for (int m = 0; m < int(small.size()); m++)
-            {
-                for (int n = 0; n < int(small[m].size()); n++)
-                {
-                    delta = big[i + m][j + n] - small[m][n];
-                    if (delta != 0)
-                    {
-                        num_delta++;
-                        sum_delta += delta;
-                    }
-                }
-            }
-            if (num_delta <= max_delta)
-            {
-                num_submatrix++;
-                min_delta = min(min_delta, abs(sum_delta));
-            }
-        }
+        string tmp;
+        cin >> tmp;
+        card_stack.push(tmp);
     }
-
-    cout << num_submatrix << endl;
-    cout << (num_submatrix == 0 ? -1 : min_delta);
+    cout << "no";
+    
+    for (int i = 0; i < 5; i++)
+    {
+        player_card['A'][i] = card_stack.front();
+        card_stack.pop();
+        player_card['B'][i] = card_stack.front();
+        card_stack.pop();
+        player_card['C'][i] = card_stack.front();
+        card_stack.pop();
+        player_card['D'][i] = card_stack.front();
+        card_stack.pop();
+    }
+    cout << "yes";
 }
