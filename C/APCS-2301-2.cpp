7f7ff3afbe28d4aac5a820ef1641f2eed59db6b0
xTaiwanPingLord <tojimmy0917@gmail.com>
2023-01-08 22:04:03 +0800

    3

--- C/APCS-2301-2.cpp
+++ C/APCS-2301-2.cpp
@@ -3,5 +3,26 @@ using namespace std;
 
 int main()
 {
-    in
+    int string_len, output_len, output_line;
+    cin >> string_len >> output_len >> output_line;
+
+    vector<string> str(output_len + 1, string(string_len, ' '));
+    cin >> str[0]; 
+
+    int index;
+    for (int i = 1; i <= output_len; i++)
+    {
+        for (int j = 0; j < string_len; j++)
+        {
+            cin >> index;
+            str[i][index - 1] = str[i - 1][j]; // 將上個字串 j 放到，新的字串 index(輸入) 的位置
+        }
+    }
+
+    for (int i = 0; i < output_line; i++)
+    {
+        for (int j = 1; j <= output_len; j++) // str[0]原始字串不用輸出，注意輸出的排列方向
+            cout << str[j][i];
+        cout << "\n";
+    }
 }
--- C/APCS-2301-3.cpp
+++ C/APCS-2301-3.cpp
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-uint64_t to_ll(const string &s)
+// declaring some function
+uint64_t f(const string &s);
+uint64_t parse(const string &s);
+uint64_t to_ll(const string &s);
+
+uint64_t to_ll(const string &s) // convert string to long long
 {
     uint64_t out = 0;
     for (int i = 0; i < (int)s.size(); i++)
@@ -14,41 +19,71 @@ uint64_t to_ll(const string &s)
 uint64_t parse(const string &s)
 {
     int type = 0, prev_type = 0;
-    int idx_r = -1, idx_l = -1;
+    int idx = -1;
+    int layer;
     int len = (int)s.size();
 
     for (int i = 0; i < len; i++)
     {
         if (s[i] == '*')
-        {
             type = 3;
-        }
         else if (s[i] == '+')
-        {
             type = 2;
-        }
         else if (s[i] == '(') // f(
         {
             type = 1;
+            layer = 0;
+            for (; i < len; i++) // 1. 解析嵌套的括號 如: ( (左右的括號跳過) ) ，以跳過f()中的+以及*，
+            {
+                if (s[i] == '(')
+                    layer++; // starting from 1 here
+                else if (s[i] == ')')
+                    layer--;
+                if (layer == 0)
+                    break;
+            }
         }
 
-        if (type > prev_type)
+        if (type > prev_type) // 找到的運算符號 優先級更高
         {
             prev_type = type;
-            idx_l = i;
-            cout << "str: " << s << " | type: " << type << " | idx(L,R): " << idx_l << ", " << idx_r << " | substr(L,R): " << s.substr(0, idx_l) << ", " << s.substr(idx_l + 1) << "\n";
-            cout.flush();
+            idx = i;
         }
     }
 
-    if (prev_type == 0)
+    if (prev_type == 0) // 如果沒有運算符號，回傳數字
         return to_ll(s);
-    else if (prev_type == 3)
-        return parse(s.substr(0, idx_l)) * parse(s.substr(idx_l + 1));
+    else if (prev_type == 3) // 切字串，分別計算運算符號前後的兩個字串
+        return parse(s.substr(0, idx)) * parse(s.substr(idx + 1));
     else if (prev_type == 2)
-        return parse(s.substr(0, idx_l)) + parse(s.substr(idx_l + 1));
+        return parse(s.substr(0, idx)) + parse(s.substr(idx + 1)); 
+    else if (prev_type == 1)
+        return f(s); // 把整個字串丟給f()處理 這邊字串只會出現 "f(1+2, 3)" 之類的，不用再切
     else
-        return 99999999;
+        return 99999999; 
+}
+
+uint64_t f(const string &s)
+{
+    uint64_t max_num = 0, min_num = INT64_MAX -1, num;
+    int idx_l = -1, idx_r = 1, len = (int)s.size();
+    int layer = 0;
+    for (int i = 2; i < len; i++) // 從f( 也就是第3個字元開始。
+    {
+        if ((s[i] == ',' || s[i] == ')') && layer == 0) // 對每個以,分隔的參數做運算，取最大最小值
+        {
+            idx_l = idx_r + 1;
+            idx_r = i;
+            num = parse(s.substr(idx_l, idx_r - idx_l));
+            max_num = max(max_num, num);
+            min_num = min(min_num, num);
+        }
+        if (s[i] == '(') // 解析嵌套的括號 (())
+            layer++;
+        else if (s[i] == ')' && layer != 0)
+            layer--;
+    }
+    return max_num - min_num;
 }
 
 int main()
